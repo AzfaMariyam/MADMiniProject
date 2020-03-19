d43060d14ca1ee8487a0da51a581e95b82416d4c
AzfaMariyam <57381624+AzfaMariyam@users.noreply.github.com>
2020-03-19 14:15:12 +0530

    MAD mini proj shipping

---
+++ app/src/main/java/com/example/madminiproject/PurchaseConfirmation.java
@@ -0,0 +1,14 @@
+package com.example.madminiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchaseConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_confirmation);
+    }
+}
