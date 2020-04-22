ec5b80ad84fc6079880230c582f60772bbd95e31
AzfaMariyam <57381624+AzfaMariyam@users.noreply.github.com>
2020-04-22 17:41:42 +0530

    Merge branch 'dev' of https://github.com/AzfaMariyam/MADMiniProject into shipping
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/madminiproject/ShippingDetHandler.java
+++ app/src/main/java/com/example/madminiproject/ShippingDetHandler.java
@@ -2,10 +2,15 @@ package com.example.madminiproject;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShippingDetHandler extends SQLiteOpenHelper {
 
@@ -34,8 +39,8 @@ public class ShippingDetHandler extends SQLiteOpenHelper {
                     ShippingTable.ship.COL_2 + " TEXT," +
                     ShippingTable.ship.COL_3 + " TEXT," +
                     ShippingTable.ship.COL_4 + " TEXT," +
-                    ShippingTable.ship.COL_5 + " INTEGER," +
-                    ShippingTable.ship.COL_6 + " INTEGER," +
+                    ShippingTable.ship.COL_5 + " TEXT," +
+                    ShippingTable.ship.COL_6 + " TEXT," +
                     ShippingTable.ship.COL_7 + " TEXT)";
 
     private static final String SQL_DELETE_ENTRIES =
@@ -52,8 +57,8 @@ public class ShippingDetHandler extends SQLiteOpenHelper {
         values.put(ShippingTable.ship.COL_2, LastName);
         values.put(ShippingTable.ship.COL_3, Address);
         values.put(ShippingTable.ship.COL_4, Country);
-        values.put(ShippingTable.ship.COL_5, Integer.parseInt(PostalCode));
-        values.put(ShippingTable.ship.COL_6, Integer.parseInt(Telno));
+        values.put(ShippingTable.ship.COL_5, PostalCode);
+        values.put(ShippingTable.ship.COL_6, Telno);
         values.put(ShippingTable.ship.COL_7, Email);
 
         // Insert the new row, returning the primary key value of the new row
@@ -61,4 +66,114 @@ public class ShippingDetHandler extends SQLiteOpenHelper {
 
         return newRowId;
     }
+
+    public List readAllInfo(String FirstName) {
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        String[] projection = {
+                BaseColumns._ID,
+                ShippingTable.ship.COL_1,
+                ShippingTable.ship.COL_2,
+                ShippingTable.ship.COL_3,
+                ShippingTable.ship.COL_4,
+                ShippingTable.ship.COL_5,
+                ShippingTable.ship.COL_6,
+                ShippingTable.ship.COL_7
+        };
+
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = ShippingTable.ship.COL_1 + " LIKE ?";
+        String[] selectionArgs = { FirstName };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                ShippingTable.ship.COL_1 + " ASC";
+
+        Cursor cursor = db.query(
+                ShippingTable.ship.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List ShippingDet = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String fname = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_1));
+            String lname = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_2));
+            String address = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_3));
+            String country = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_4));
+            String pcode = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_5));
+            String telno = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_6));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow(ShippingTable.ship.COL_7));
+
+
+            ShippingDet.add(fname);
+            ShippingDet.add(lname);
+            ShippingDet.add(address);
+            ShippingDet.add(country);
+            ShippingDet.add(pcode);
+            ShippingDet.add(telno);
+            ShippingDet.add(email);
+
+        }
+        cursor.close();
+        return ShippingDet;
+    }
+
+
+    public Boolean updateShipping (String FirstName, String LastName, String Address, String Country, String PostalCode, String Telno, String Email){
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(ShippingTable.ship.COL_2, LastName);
+        values.put(ShippingTable.ship.COL_3, Address);
+        values.put(ShippingTable.ship.COL_4, Country);
+        values.put(ShippingTable.ship.COL_5, PostalCode);
+        values.put(ShippingTable.ship.COL_6, Telno);
+        values.put(ShippingTable.ship.COL_7, Email);
+
+
+        // Which row to update, based on the title
+        String selection = ShippingTable.ship.COL_1 + " LIKE ?";
+        String[] selectionArgs = { FirstName };
+
+        int count = db.update(
+                ShippingTable.ship.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
+        if (count >=1 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+    public void deleteShip (String FirstName){
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        // Define 'where' part of query.
+        String selection = ShippingTable.ship.COL_1 + " LIKE ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = { FirstName };
+        // Issue SQL statement.
+        int deletedRows = db.delete(ShippingTable.ship.TABLE_NAME, selection, selectionArgs);
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/madminiproject/ShippingUpdate.java
+++ app/src/main/java/com/example/madminiproject/ShippingUpdate.java
@@ -2,17 +2,112 @@ package com.example.madminiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class ShippingUpdate extends AppCompatActivity {
-
     EditText fname, lname, address, country, pcode, tel, email;
     EditText search;
+    Button save, del, searchbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipping_update);
+
+        fname = findViewById(R.id.firstname);
+        lname = findViewById(R.id.lastname);
+        address = findViewById(R.id.addr);
+        country = findViewById(R.id.country);
+        pcode = findViewById(R.id.pcode);
+        tel = findViewById(R.id.telno);
+        email = findViewById(R.id.email);
+        search = findViewById(R.id.shipsearch);
+
+        save = findViewById(R.id.updateshipbtn);
+        del = findViewById(R.id.delship);
+        searchbtn = findViewById(R.id.searchShip);
+
+
+        searchbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ShippingDetHandler newdb = new ShippingDetHandler(getApplicationContext());
+
+                List firstN = newdb.readAllInfo(search.getText().toString());
+
+                if (firstN.isEmpty()){
+                    Toast.makeText(ShippingUpdate.this, "No Details", Toast.LENGTH_SHORT).show();
+                    search.setText(null);
+                }
+                else {
+
+                    Toast.makeText(ShippingUpdate.this, "Details Found! Details: "+firstN.get(0).toString(), Toast.LENGTH_SHORT).show();
+                    search.setText(firstN.get(0).toString());
+                    lname.setText(firstN.get(1).toString());
+                    address.setText(firstN.get(2).toString());
+                    country.setText(firstN.get(3).toString());
+                    pcode.setText(firstN.get(4).toString());
+                    tel.setText(firstN.get(5).toString());
+                    email.setText(firstN.get(6).toString());
+
+
+                }
+
+            }
+        });
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ShippingDetHandler newdb = new ShippingDetHandler(getApplicationContext());
+
+                Boolean status = newdb.updateShipping(search.getText().toString(),lname.getText().toString(),address.getText().toString(),country.getText().toString(), pcode.getText().toString(),tel.getText().toString(),email.getText().toString() );
+                if (status){
+                    Toast.makeText(ShippingUpdate.this, "Shipping Details Updated", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(),PaymentDetails.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(ShippingUpdate.this, "Shipping details update Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ShippingDetHandler det = new ShippingDetHandler(getApplicationContext());
+                det.deleteShip(search.getText().toString());
+
+                Toast.makeText(ShippingUpdate.this, "Shipping Details Deleted", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(getApplicationContext(),PaymentDetails.class);
+                startActivity(i);
+
+                search.setText(null);
+                lname.setText(null);
+                address.setText(null);
+                country.setText(null);
+                pcode.setText(null);
+                tel.setText(null);
+                email.setText(null);
+
+            }
+        });
+
+
     }
 }
